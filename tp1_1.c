2b06afd62f46513be3cee15c99e90e9c1545ce10
Gonzalo Martinez <mgonzalo192@gmail.com>
2023-03-21 11:40:27 -0300

    subido el punto 3 modificado con el resto de las respuestas

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    
-    printf("Hola Mundo!");
+
+    int numero = 21;
+    int *puntero = &numero;
+
+    printf("\nHola Mundo!\n\n");
+    printf("La direccion de memoria guardada en el puntero es %p\n\n",puntero);
+    printf("La direccion de memoria de mi variable es %p\n\n",&numero);
+    printf("La direccion de memoria del puntero es %p\n\n",&puntero);
+    printf("La variable ocupa %d bytes de memoria\n\n",sizeof(numero));
+    printf("El puntero contiene el numero %d\n\n",*puntero);
 
     return 0;
 }
